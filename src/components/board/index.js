57d36b26a12c89f9ef22cbdb8a79c619b758d375
Alexander Wilkinson <alexwilkinson@gmail.com>
2022-07-13 11:45:32 +0800

    added in board structure and style next a bit more style improvements and then work on win conditions

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,28 +1,66 @@
-import React from 'react';
-import './index.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectCell } from '../../store/actions/moves';
+import React from "react";
+import "./index.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectCell } from "../../store/actions/moves";
 
-const selectBoard = (state) => state.board
-const selectGame = (state) => state.game
+const BoardColumn = ({ column, columnIndex, handlePlay }) => {
+  return (
+    <div className="board-column">
+      {column.map((cell, cellIndex) => {
+        return (
+          <div
+            key={cellIndex}
+            className="board-cell"
+            onClick={() => handlePlay(columnIndex, cellIndex)}
+          >
+            <div className="board-cell-contents">{cell ?? "click"}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-const getRandomCell = (i) => Math.floor(Math.random() * i)
+const selectBoard = (state) => state.board;
+const selectGame = (state) => state.game;
+
+const getRandomCell = (i) => Math.floor(Math.random() * i);
 
 export const Board = () => {
-  const board = useSelector(selectBoard)
-  const game = useSelector(selectGame)
-  const dispatch = useDispatch()
+  const board = useSelector(selectBoard);
+  const game = useSelector(selectGame);
+  const dispatch = useDispatch();
+
+  const handlePlay = (currentPlayer) => (rowNumber, columnNumber) => {
+    dispatch(selectCell(currentPlayer, rowNumber, columnNumber));
+  };
 
   return (
     <div className="Board">
-      Board: { JSON.stringify(board) }
-      <div onClick={() => dispatch(
-        selectCell(
-          game.currentPlayer,
-          getRandomCell(board.length),
-          getRandomCell(board.length)
-        )
-      )}>Player {game.currentPlayer}</div>
+      {/* Board: {JSON.stringify(board)} */}
+      <div className="game-board">
+        {board.map((col, i) => (
+          <BoardColumn
+            key={Date.now() + i}
+            column={col}
+            columnIndex={i}
+            handlePlay={handlePlay(game.currentPlayer)}
+          />
+        ))}
+      </div>
+      <div
+        onClick={() =>
+          dispatch(
+            selectCell(
+              game.currentPlayer,
+              getRandomCell(board.length),
+              getRandomCell(board.length)
+            )
+          )
+        }
+      >
+        Player {game.currentPlayer}
+      </div>
     </div>
-  )
-}
+  );
+};
