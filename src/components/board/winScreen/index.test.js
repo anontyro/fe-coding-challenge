282512065144d4ed5fd5e61d6bf6b23c641b588a
Alexander Wilkinson <alexwilkinson@gmail.com>
2022-07-14 20:39:18 +0800

    added winScreen tests

---
+++ src/components/board/winScreen/index.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import WinScreen from ".";
+
+test("will correctly render out the win screen", () => {
+  const winner = "X";
+  render(<WinScreen winner={winner} resetGame={() => {}} />);
+
+  expect(screen.getByText(/Winner: X/i)).toBeInTheDocument();
+  expect(
+    screen.getByRole("button", { name: "Play Again" })
+  ).toBeInTheDocument();
+});
+
+test("will correctly fire the play again button when pressed", async () => {
+  let pressed = 0;
+  const winner = "X";
+  const resetGame = () => pressed++;
+  render(<WinScreen winner={winner} resetGame={resetGame} />);
+
+  await fireEvent.click(screen.getByRole("button", { name: "Play Again" }));
+  expect(pressed).toBe(1);
+});
